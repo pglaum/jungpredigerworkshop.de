66b61241c19fc8637e054d28792cb24e9bc4f647
Philipp Glaum <philipp.glaum@sinetcon.com>
2023-07-10 10:03:25 +0200

    seo stuff

--- pages/index.vue
+++ pages/index.vue
@@ -17,3 +17,19 @@
         <landing-footer />
     </div>
 </template>
+
+<script setup lang="ts">
+const title = 'Jungprediger Workshop'
+const description = 'Der Jungprediger-Workshop soll Pastoren in ihren ersten Dienstjahren ' +
+    'und auch angehenden Pastoren und Laienpredigern helfen, Gottes Wort der Wahrheit ' +
+    '„recht auszuteilen“.'
+
+useSeoMeta({
+    title,
+    ogTitle: title,
+    description,
+    ogDescription: description,
+    ogImage: '/img/favicon.png',
+    twitterCard: 'summary_large_image',
+})
+</script>
