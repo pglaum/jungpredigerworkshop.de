4d870ae0ca6236bfdde14325fffac6cd104112a1
Philipp Glaum <philipp.glaum@sinetcon.com>
2024-05-31 16:54:49 +0200

    link to my site

--- components/landing/footer.vue
+++ components/landing/footer.vue
@@ -2,7 +2,10 @@
     <div class="relative isolate overflow-hidden bg-accent pb-20 text-primary-content">
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
             <div class="text-center">
-                &copy; 2023 Philipp Glaum
+                &copy; 2024
+                <NuxtLink class="underline underline-offset-4 hover:no-underline" href="https://pglaum.de">
+                    Philipp Glaum
+                </NuxtLink>
             </div>
         </div>
     </div>
